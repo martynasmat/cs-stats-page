5be5eaef691060c411105c5ec1606cade5dcc2b6
martynasmat <martynasmat03@gmail.com>
2025-08-04 04:29:09 +0300

    add sudo to subprocess.run

--- backend/main.py
+++ backend/main.py
@@ -282,7 +282,7 @@ def redeploy() -> tuple[str, int]:
     if not verify_signature(payload, header_signature):
         abort(403, "Signature verification failed")
 
-    subprocess.run(["/bin/bash", "/home/cs-stats-page/backend/deploy_script.sh"])
+    subprocess.run(["/bin/bash", "/usr/bin/sudo", "/home/cs-stats-page/backend/deploy_script.sh"])
     return "Webhook received and verified", 200
 
 
